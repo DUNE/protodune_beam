88c9ecd79ac65845c5144084a761d3ed22364274
np04 daq <np04daq@np04-srv-003.cern.ch>
2018-09-05 13:39:40 +0200

    readdition of 4hr script

--- RunScripts/run_AnaTest.sh
+++ RunScripts/run_AnaTest.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-newFileList=`cat /scratch/BeamInstOutput/Trees_DIPRaw/fileList_Test.txt`
+newFileList=`cat /scratch/BeamInstOutput/Trees_DIPRaw/180830_Testing/files.list`
 detectorList=`cat /nfs/sw/BeamLineMonitoring/DIPExtraction/SubscriptionLists/detectors`
-/nfs/sw/BeamLineMonitoring/AnaScripts/AnaTest.exe true $newFileList $detectorList
+/nfs/sw/BeamLineMonitoring/AnaScripts/AnaTest.exe true /scratch/BeamInstOutput/Trees_DIPRaw/180901-180902_Testing/eventsData_1535871342.root $detectorList
